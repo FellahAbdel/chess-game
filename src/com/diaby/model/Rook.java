144ad54cee27f404b407f43df0d172edbc249ab7
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-04-25 19:34:35 +0200

    Supprime break dans les boucles

--- src/com/diaby/model/Rook.java
+++ src/com/diaby/model/Rook.java
@@ -46,69 +46,57 @@ public class Rook extends ChessPiece {
 
    }
 
-    private void checkMoves(int row, int col, int limit, int increment, ChessPiece[][] board, ArrayList<int[]> moves) {
-        boolean canMove = true;
-        while (canMove && row != limit && col != limit) {
-            if (board[row][col] == null) {
-                moves.add(new int[]{row, col});
-            } else if (board[row][col].isWhite() != this.isWhite()) {
-                moves.add(new int[]{row, col});
-                canMove = false;
-            } else {
-                canMove = false;
-            }
-            row += increment;
-            col += increment;
-        }
-    }
     public ArrayList<int[]> possiblesMoves(int startYRow, int startXCol, ChessPiece[][] board) {
         ArrayList<int[]> moves = new ArrayList<>();
-
+        boolean canMove = true ;
         // Check moves to the right
-        for (int i = startXCol + 1; i < 8; i++) {
+        for (int i = startXCol + 1; i < 8 && canMove; i++) {
             if (board[startYRow][i] == null) {
                 moves.add(new int[]{startYRow, i});
             } else if (board[startYRow][i].isWhite() != this.isWhite()) {
                 moves.add(new int[]{startYRow, i});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false ;
             }
         }
 
+        canMove = true ;
         // Check moves to the left
-        for (int i = startXCol - 1; i >= 0; i--) {
+        for (int i = startXCol - 1; i >= 0 && canMove; i--) {
             if (board[startYRow][i] == null) {
                 moves.add(new int[]{startYRow, i});
             } else if (board[startYRow][i].isWhite() != this.isWhite()) {
                 moves.add(new int[]{startYRow, i});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
+        canMove = true ;
         // Check moves to the bottom
-        for (int i = startYRow + 1; i < 8; i++) {
+        for (int i = startYRow + 1; i < 8 && canMove; i++) {
             if (board[i][startXCol] == null) {
                 moves.add(new int[]{i, startXCol});
             } else if (board[i][startXCol].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, startXCol});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false ;
             }
         }
 
+        canMove = true ;
         // Check moves to the top
-        for (int i = startYRow - 1; i >= 0; i--) {
+        for (int i = startYRow - 1; i >= 0 && canMove; i--) {
             if (board[i][startXCol] == null) {
                 moves.add(new int[]{i, startXCol});
             } else if (board[i][startXCol].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, startXCol});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
