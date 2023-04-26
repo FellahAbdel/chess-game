76c12cf36da4d0167468b152f61da953ebd8bc69
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2023-04-26 18:20:30 +0200

    le roi ne se déplace au position ménacé

--- src/com/diaby/model/King.java
+++ src/com/diaby/model/King.java
@@ -1,7 +1,10 @@
 package com.diaby.model;
 
+import com.diaby.view.ChessBoardView;
+
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class King extends ChessPiece {
     private boolean castlingDone = false;
@@ -41,9 +44,7 @@ public class King extends ChessPiece {
         if (deltaX <= 1 && deltaY <= 1)
         {
             // check if the King is not moving to a threatened position
-            if (isPositionThreatened(board, endX, endY, this.isWhite())) {
-                return true;
-            }
+
             // Vérifie si la case de destination est vide ou occupée par une pièce de la couleur opposée
             if (board[endY][endX] == null || !board[endY][endX].getColor().equals(getColor())) {
                 return true;
@@ -53,72 +54,6 @@ public class King extends ChessPiece {
         return false;
     }
 
-    private boolean isPositionThreatened(ChessPiece[][] board, int x, int y, boolean isWhite) {
-        // check if any opposing Knight is threatening the position
-        int[][] knightMoves = {{-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}};
-        for (int[] move : knightMoves) {
-            int x2 = x + move[0];
-            int y2 = y + move[1];
-            if (isValidPosition(x2, y2)) {
-                ChessPiece piece = board[x2][y2];
-                if (piece instanceof Knight && piece.isWhite() != isWhite) {
-                    return false;
-                }
-            }
-        }
-
-        // check if any opposing Pawn is threatening the position
-        int[][] pawnMoves = isWhite ? new int[][]{{-1, -1}, {-1, 1}} : new int[][]{{1, -1}, {1, 1}};
-        for (int[] move : pawnMoves) {
-            int x2 = x + move[0];
-            int y2 = y + move[1];
-            if (isValidPosition(x2, y2)) {
-                ChessPiece piece = board[x2][y2];
-                if (piece instanceof Pawn && piece.isWhite() != isWhite) {
-                    return false;
-                }
-            }
-        }
-
-        // check if any opposing Rook, Bishop, or Queen is threatening the position
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
-        for (int[] direction : directions) {
-            int x2 = x + direction[0];
-            int y2 = y + direction[1];
-            while (isValidPosition(x2, y2)) {
-                ChessPiece piece = board[x2][y2];
-                if (piece != null) {
-                    if (piece.isWhite() == isWhite) {
-                        break;
-                    } else {
-                        if ((piece instanceof Rook || piece instanceof Queen) && (direction[0] == 0 || direction[1] == 0)) {
-                            return false;
-                        } else if ((piece instanceof Bishop || piece instanceof Queen) && direction[0] * direction[1] != 0) {
-                            return false;
-                        }
-                        break;
-                    }
-                }
-                x2 += direction[0];
-                y2 += direction[1];
-            }
-        }
-
-        // check if any opposing King is threatening the position
-        int[][] kingMoves = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-        for (int[] move : kingMoves) {
-            int x2 = x + move[0];
-            int y2 = y + move[1];
-            if (isValidPosition(x2, y2)) {
-                ChessPiece piece = board[x2][y2];
-                if (piece instanceof King && piece.isWhite() != isWhite) {
-                    return false;
-                }
-            }
-        }
-        // no opposing piece is threatening the position
-        return true;
-    }
 
     private boolean isValidPosition(int x, int y) {
         return x >= 0 && x < 8 && y >= 0 && y < 8;
@@ -126,11 +61,11 @@ public class King extends ChessPiece {
 
     public ArrayList<int[]> possiblesMoves(int startYRow, int startXCol, ChessPiece[][] board) {
         ArrayList<int[]> moves = new ArrayList<>();
-
+        King king = (King) board[startYRow][startXCol];
         // Check moves to the right
         if (startXCol + 1 < 8) {
             ChessPiece piece = board[startYRow][startXCol + 1];
-            if (piece == null || piece.getColor() != this.getColor()) {
+            if (piece == null || piece.isWhite() != this.isWhite()) {
                 // Try the move and check if king is in check
                 ChessPiece[][] testBoard = ChessBoard.copyBoard(board);
                 testBoard[startYRow][startXCol + 1] = testBoard[startYRow][startXCol];
@@ -144,7 +79,7 @@ public class King extends ChessPiece {
         // Check moves to the left
         if (startXCol - 1 >= 0) {
             ChessPiece piece = board[startYRow][startXCol - 1];
-            if (piece == null || piece.getColor() != this.getColor()) {
+            if (piece == null || piece.isWhite() != this.isWhite()) {
                 // Try the move and check if king is in check
                 ChessPiece[][] testBoard = ChessBoard.copyBoard(board);
                 testBoard[startYRow][startXCol - 1] = testBoard[startYRow][startXCol];
@@ -158,7 +93,7 @@ public class King extends ChessPiece {
         // Check moves to the bottom
         if (startYRow + 1 < 8) {
             ChessPiece piece = board[startYRow + 1][startXCol];
-            if (piece == null || piece.getColor() != this.getColor()) {
+            if (piece == null || piece.isWhite() != this.isWhite()) {
                 // Try the move and check if king is in check
                 ChessPiece[][] testBoard = ChessBoard.copyBoard(board);
                 testBoard[startYRow + 1][startXCol] = testBoard[startYRow][startXCol];
@@ -172,7 +107,7 @@ public class King extends ChessPiece {
         // Check moves to the top
         if (startYRow - 1 >= 0) {
             ChessPiece piece = board[startYRow - 1][startXCol];
-            if (piece == null || piece.getColor() != this.getColor()) {
+            if (piece == null || piece.isWhite() != this.isWhite()) {
                 // Try the move and check if king is in check
                 ChessPiece[][] testBoard = ChessBoard.copyBoard(board);
                 testBoard[startYRow - 1][startXCol] = testBoard[startYRow][startXCol];
@@ -186,7 +121,7 @@ public class King extends ChessPiece {
         // Check diagonal moves to the top-right
         if (startYRow - 1 >= 0 && startXCol + 1 < 8) {
             ChessPiece piece = board[startYRow - 1][startXCol + 1];
-            if (piece == null || piece.getColor() != this.getColor()) {
+            if (piece == null || piece.isWhite() != this.isWhite()) {
                 // Try the move and check if king is in check
                 ChessPiece[][] testBoard = ChessBoard.copyBoard(board);
                 testBoard[startYRow - 1][startXCol + 1] = testBoard[startYRow][startXCol];
@@ -200,7 +135,7 @@ public class King extends ChessPiece {
         // Check diagonal moves to the top-left
         if (startYRow - 1 >= 0 && startXCol - 1 >= 0) {
             ChessPiece piece = board[startYRow - 1][startXCol - 1];
-            if (piece == null || piece.getColor() != this.getColor()) {
+            if (piece == null || piece.isWhite() != this.isWhite()) {
                 // Try the move and check if king is in check
                 ChessPiece[][] testBoard = ChessBoard.copyBoard(board);
                 testBoard[startYRow - 1][startXCol - 1] = testBoard[startYRow][startXCol];
@@ -215,7 +150,7 @@ public class King extends ChessPiece {
         // Check diagonal moves to the bottom-right
         if (startYRow + 1 < 8 && startXCol + 1 < 8) {
             ChessPiece piece = board[startYRow + 1][startXCol + 1];
-            if (piece == null || piece.getColor() != this.getColor()) {
+            if (piece == null || piece.isWhite() != this.isWhite()) {
                 // Try the move and check if king is in check
                 ChessPiece[][] testBoard = ChessBoard.copyBoard(board);
                 testBoard[startYRow + 1][startXCol + 1] = testBoard[startYRow][startXCol];
@@ -229,7 +164,7 @@ public class King extends ChessPiece {
         // Check diagonal moves to the bottom-left
         if (startYRow + 1 < 8 && startXCol - 1 >= 0) {
             ChessPiece piece = board[startYRow + 1][startXCol - 1];
-            if (piece == null || piece.getColor() != this.getColor()) {
+            if (piece == null || piece.isWhite() != this.isWhite()) {
                 // Try the move and check if king is in check
                 ChessPiece[][] testBoard = ChessBoard.copyBoard(board);
                 testBoard[startYRow + 1][startXCol - 1] = testBoard[startYRow][startXCol];
@@ -251,16 +186,8 @@ public class King extends ChessPiece {
                     isInCheck = true;
                 }
                 if (!isInCheck) {
-                    ChessPiece arrivalCase = board[startYRow][startXCol + 2];
-                    if (arrivalCase == null || arrivalCase.getColor() != this.getColor()) {
-                        // Try the move and check if king is in check
-                        ChessPiece[][] testBoard = ChessBoard.copyBoard(board);
-                        testBoard[startYRow][startXCol + 2] = testBoard[startYRow][startXCol];
-                        testBoard[startYRow][startXCol] = null;
-                        if (!isInCheck(this.isWhite(), testBoard)) {
-                            moves.add(new int[]{startYRow, startXCol + 2});
-                        }
-                    }
+
+                    moves.add(new int[]{startYRow, startXCol + 2});
                 }
             }
         }
@@ -275,19 +202,12 @@ public class King extends ChessPiece {
                     isInCheck = true;
                 }
                 if (!isInCheck) {
-                    ChessPiece arrivalCase = board[startYRow][startXCol - 2];
-                    if (arrivalCase == null || arrivalCase.getColor() != this.getColor()) {
-                        // Try the move and check if king is in check
-                        ChessPiece[][] testBoard = ChessBoard.copyBoard(board);
-                        testBoard[startYRow][startXCol - 2] = testBoard[startYRow][startXCol];
-                        testBoard[startYRow][startXCol] = null;
-                        if (!isInCheck(this.isWhite(), testBoard)) {
-                            moves.add(new int[] {startYRow, startXCol - 2});
-                        }
-                    }
+                    moves.add(new int[] {startYRow, startXCol - 2});
                 }
             }
         }
+
+
 
         return moves;
     }
@@ -309,7 +229,7 @@ public class King extends ChessPiece {
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 ChessPiece piece = board[row][col];
-                if (piece != null && piece.isWhite() != isWhiteKing) {
+                if (piece != null && piece.isWhite() != isWhiteKing && !(piece instanceof King)) {
                     ArrayList<int[]> moves = piece.possiblesMoves(row, col, board);
                     for (int[] move : moves) {
                         if (kingPos != null && move[0] == kingPos[0] && move[1] == kingPos[1]) {
@@ -317,6 +237,7 @@ public class King extends ChessPiece {
                         }
                     }
                 }
+
             }
         }
 
