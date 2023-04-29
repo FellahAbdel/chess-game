0c9ce20a0327266efb800d3f83ce4ef9e1735475
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2023-04-29 22:11:32 +0200

    modification GameController , suppression de la boucle infinie , pas besoin

--- src/com/diaby/controller/GameController.java
+++ src/com/diaby/controller/GameController.java
@@ -1,84 +1,19 @@
 package com.diaby.controller;
-// package com.diaby;
-//
-//public class GameController {
-//    protected ChessBoard board;
-//    protected BoardView boardView;
-//    protected ChessPiece selectedPiece;
-//
-//    public GameController(ChessBoard board, BoardView boardView) {
-//        this.board = board;
-//        this.boardView = boardView;
-//    }
-//
-//    public void play() {
-//        // Boucle de jeu
-//        while (!RegleDuJeu.estFinDeJeu(board)) {
-//            // Afficher le plateau de jeu
-//            boardView.display(board);
-//
-//            // Demander à l'utilisateur de sélectionner une pièce
-//            int[] startCoords = boardView.getPieceSelection();
-//
-//            // Vérifier si une pièce est présente à la position sélectionnée
-//            if (!board.isPieceAt(startCoords[0], startCoords[1])) {
-//                boardView.displayErrorMessage("Aucune pièce sélectionnée. Veuillez réessayer.");
-//                continue;
-//            }
-//
-//            // Récupérer la pièce sélectionnée
-//            selectedPiece = board.getPieceAt(startCoords[0], startCoords[1]);
-//
-//            // Vérifier si la pièce appartient au joueur en cours
-//            if (selectedPiece.getColor() != currentPlayerColor) {
-//                boardView.displayErrorMessage("Cette pièce n'appartient pas au joueur en cours. Veuillez réessayer.");
-//                continue;
-//            }
-//
-//            // Demander à l'utilisateur de sélectionner une position d'arrivée
-//            int[] endCoords = boardView.getMoveSelection();
-//
-//            // Vérifier si le déplacement est valide
-//            if (!RegleDuJeu.estDeplacementValide(selectedPiece, endCoords[0], endCoords[1], board)) {
-//                boardView.displayErrorMessage("Déplacement invalide. Veuillez réessayer.");
-//                continue;
-//            }
-//
-//            // Déplacer la pièce sur le plateau de jeu
-//            board.movePiece(startCoords[0], startCoords[1], endCoords[0], endCoords[1]);
-//
-//            // Passer au joueur suivant
-//            currentPlayerColor = currentPlayerColor == Color.WHITE ? Color.BLACK : Color.WHITE;
-//        }
-//
-//        // Afficher le message de fin de jeu
-//        boardView.displayEndOfGameMessage();
-//    }
-//}
-//
-
-import com.diaby.model.ChessBoard;
 import com.diaby.view.ChessBoardView;
 
-public class GameController {
 
-    private ChessBoard chessBoard;
+public class GameController {
     private ChessBoardView chessBoardView;
 
-    public GameController() {
-        /*chessBoard = new ChessBoard();*/
-        chessBoardView = new ChessBoardView();
+    public GameController(ChessBoardView chessBoardView) {
+        this.chessBoardView = chessBoardView;
     }
 
     public void startGame() {
-        chessBoardView.displayBoard();
-        ChessBoard chessBoard = chessBoardView.getBoard();
-        while (!chessBoard.isGameOver()) {
-            // TODO: Implement game logic here
-        }
-        chessBoardView.displayGameOver();
-    }
 
-    // TODO: Add additional methods for handling user input and game logic
+        // afficher plateau jeu
+        chessBoardView.displayBoard();
+
+    }
 
 }
