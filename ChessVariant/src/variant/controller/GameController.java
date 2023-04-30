1229629f2a24c9ea636d40fe14d8297492368c78
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2023-04-30 16:24:25 +0200

    ajout classe GameController dans ChessVariant

---
+++ ChessVariant/src/variant/controller/GameController.java
@@ -0,0 +1,18 @@
+package variant.controller;
+import variant.view.ChessBoardView;
+
+public class GameController {
+    private ChessBoardView chessBoardView;
+
+    public GameController(ChessBoardView chessBoardView) {
+        this.chessBoardView = chessBoardView;
+    }
+
+    public void startGame() {
+
+        // afficher plateau jeu
+        chessBoardView.displayBoard();
+
+    }
+
+}
