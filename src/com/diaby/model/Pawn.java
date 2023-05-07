f2e4551bfcc1d7cf974fa46fc105b16a635cc94d
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2023-05-07 14:43:46 +0200

    intégration d'un booléan afin de pallier au problème en passant

--- src/com/diaby/model/Pawn.java
+++ src/com/diaby/model/Pawn.java
@@ -4,58 +4,23 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class Pawn extends ChessPiece {
-    private boolean hasMoved; // indique si le pion a déjà été déplacé ou non
 
+    private boolean hasJustMoveDouble;
     public Pawn(Color color, int row, int col, boolean isWhiteTurn) {
         super("Pawn", color, row, col, isWhiteTurn);
-        hasMoved = false;
+        hasJustMoveDouble = false;
+    }
+
+    public boolean getHasJustMoveDouble() {
+        return hasJustMoveDouble;
+    }
+
+    public void setHasJustMoveDouble(boolean hasJustMoveDouble) {
+        this.hasJustMoveDouble = hasJustMoveDouble;
     }
 
     public Pawn(ChessPiece piece) {
         super(piece.getPieceName(), piece.getColor(), piece.getRow(), piece.getCol(), piece.getWhiteTurn());
-    }
-
-    public boolean getHasMoved() {
-        return hasMoved;
-    }
-
-    public void setHasMoved(boolean hasMoved) {
-        this.hasMoved = hasMoved;
-    }
-
-
-    @Override
-    public boolean isValidMove(int startYRow, int startXCol, int endYRow, int endXCol, ChessPiece[][] board) {
-        // Vérifie si le déplacement est vertical
-        int deltaX = endXCol - startXCol;
-        int deltaY = endYRow - startYRow;
-        if (deltaX != 0) {
-            return false;
-        }
-
-        // Vérifie si le déplacement est d'une case vers l'avant en fonction de la couleur
-        if (isWhite()) {
-            // Vérifie si le pion n'a pas encore été déplacé et s'il se déplace de deux cases vers l'avant
-            if (!hasMoved && deltaY == 2 && board[startYRow + 1][startXCol] == null) {
-                return true;
-            }
-            // Vérifie si le pion se déplace d'une case vers l'avant
-            if (deltaY == 1 && board[endYRow][endXCol] == null) {
-                return true;
-            }
-        } else {
-            // Vérifie si le pion n'a pas encore été déplacé et s'il se déplace de deux cases vers l'avant
-            if (!hasMoved && deltaY == -2 && board[startYRow - 1][startXCol] == null) {
-                return true;
-            }
-            // Vérifie si le pion se déplace d'une case vers l'avant
-            if (deltaY == -1 && board[endYRow][endXCol] == null) {
-                return true;
-            }
-        }
-
-        // Si aucun des cas précédents n'est vérifié, le mouvement n'est pas valide
-        return false;
     }
 
     public void promotePawn(Pawn pawn, int row, int col, String pieceType, ChessPiece[][] board) {
@@ -110,6 +75,7 @@ public class Pawn extends ChessPiece {
                 startYRow > 0 &&
                 board[startXCol][startYRow - 1] != null &&
                 board[startXCol][startYRow - 1].isWhite() == isWhite &&
+                ((Pawn)board[startXCol][startYRow + 1]).getHasJustMoveDouble() &&
                 board[startXCol + 1][startYRow - 1] == null &&
                 board[startXCol][startYRow - 1] instanceof Pawn) {
             moves.add(new int[]{startXCol + 1, startYRow - 1});
@@ -118,6 +84,7 @@ public class Pawn extends ChessPiece {
                 startYRow < 7 &&
                 board[startXCol][startYRow + 1] != null &&
                 board[startXCol][startYRow + 1].isWhite() == isWhite &&
+                ((Pawn)board[startXCol][startYRow + 1]).getHasJustMoveDouble() &&
                 board[startXCol + 1][startYRow + 1] == null &&
                 board[startXCol][startYRow + 1] instanceof Pawn) {
             moves.add(new int[]{startXCol + 1, startYRow + 1});
@@ -148,6 +115,7 @@ public class Pawn extends ChessPiece {
         if (startXCol == 3 && startYRow > 0 &&
                 board[startXCol][startYRow - 1] != null &&
                 board[startXCol][startYRow - 1].isWhite() == isWhite &&
+                ((Pawn)board[startXCol][startYRow - 1]).getHasJustMoveDouble() &&
                 board[startXCol - 1][startYRow - 1] == null &&
                 board[startXCol][startYRow - 1] instanceof Pawn) {
             moves.add(new int[]{startXCol - 1, startYRow - 1});
@@ -155,18 +123,19 @@ public class Pawn extends ChessPiece {
         if (startXCol == 3 && startYRow < 7 &&
                 board[startXCol][startYRow + 1] != null &&
                 board[startXCol][startYRow + 1].isWhite() == isWhite &&
+                ((Pawn)board[startXCol][startYRow + 1]).getHasJustMoveDouble() &&
                 board[startXCol - 1][startYRow + 1] == null &&
                 board[startXCol][startYRow + 1] instanceof Pawn) {
             moves.add(new int[]{startXCol - 1, startYRow + 1});
         }
     }
 
-    public ArrayList<int[]> possiblesMovesTurn(int startXCol, int startYRow, ChessPiece[][] board,
+    private ArrayList<int[]> possiblesMovesTurn(int startXCol, int startYRow, ChessPiece[][] board,
                                                ArrayList<int[]> moves, Boolean whiteTurn) {
         if (whiteTurn) {
             if (this.isWhite()) {  // Descente blanche.
                 descent(startXCol, startYRow, board, moves, false);
-            } else { // Montée Noir.
+            } else { // Monté Noir.
                 climb(startXCol, startYRow, board, moves, true);
             }
         } else {
@@ -179,13 +148,12 @@ public class Pawn extends ChessPiece {
 
         return moves;
     }
-
+    @Override
     public ArrayList<int[]> possiblesMoves(int startXCol, int startYRow, ChessPiece[][] board) {
         ArrayList<int[]> moves = new ArrayList<>();
 
-        moves = possiblesMovesTurn(startXCol, startYRow, board, moves, !this.getWhiteTurn());
+       return possiblesMovesTurn(startXCol, startYRow, board, moves, !this.getWhiteTurn());
 
-        return moves;
     }
 
 
