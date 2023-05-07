6d8391c0c494743cd1d3a87315b89b11393f73e0
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2023-05-07 15:52:00 +0200

    isWhiteTurn -> white_pieces_at_bottom

--- src/com/diaby/model/ChessBoard.java
+++ src/com/diaby/model/ChessBoard.java
@@ -11,34 +11,34 @@ public class ChessBoard {
         tileBoard = new ChessPiece[8][8];
     }
 
-    public void initialize(boolean isWhiteTurn) {
-        Color color = isWhiteTurn ? Color.BLACK : Color.WHITE;
-        Color oppositeColor = isWhiteTurn ? Color.WHITE : Color.BLACK;
+    public void initialize(boolean white_pieces_at_bottom) {
+        Color color = white_pieces_at_bottom ? Color.BLACK : Color.WHITE;
+        Color oppositeColor = white_pieces_at_bottom ? Color.WHITE : Color.BLACK;
 
         // Initialiser les pièces blanches
-        tileBoard[0][0] = new Rook(color, 0, 0, isWhiteTurn);
-        tileBoard[0][1] = new Knight(color, 0, 1, isWhiteTurn);
-        tileBoard[0][2] = new Bishop(color, 0, 2, isWhiteTurn);
-        tileBoard[0][3] = new Queen(color, 0, 3, isWhiteTurn);
-        tileBoard[0][4] = new King(color, 0, 4, isWhiteTurn);
-        tileBoard[0][5] = new Bishop(color, 0, 5, isWhiteTurn);
-        tileBoard[0][6] = new Knight(color, 0, 6, isWhiteTurn);
-        tileBoard[0][7] = new Rook(color, 0, 7, isWhiteTurn);
+        tileBoard[0][0] = new Rook(color, 0, 0, white_pieces_at_bottom);
+        tileBoard[0][1] = new Knight(color, 0, 1, white_pieces_at_bottom);
+        tileBoard[0][2] = new Bishop(color, 0, 2, white_pieces_at_bottom);
+        tileBoard[0][3] = new Queen(color, 0, 3, white_pieces_at_bottom);
+        tileBoard[0][4] = new King(color, 0, 4, white_pieces_at_bottom);
+        tileBoard[0][5] = new Bishop(color, 0, 5, white_pieces_at_bottom);
+        tileBoard[0][6] = new Knight(color, 0, 6, white_pieces_at_bottom);
+        tileBoard[0][7] = new Rook(color, 0, 7, white_pieces_at_bottom);
         for (int j = 0; j < 8; j++) {
-            tileBoard[1][j] = new Pawn(color, 1, j, isWhiteTurn);
+            tileBoard[1][j] = new Pawn(color, 1, j, white_pieces_at_bottom);
         }
 
         // Initialiser les pièces noires
-        tileBoard[7][0] = new Rook(oppositeColor, 7, 0, isWhiteTurn);
-        tileBoard[7][1] = new Knight(oppositeColor, 7, 1, isWhiteTurn);
-        tileBoard[7][2] = new Bishop(oppositeColor, 7, 2, isWhiteTurn);
-        tileBoard[7][3] = new Queen(oppositeColor, 7, 3, isWhiteTurn);
-        tileBoard[7][4] = new King(oppositeColor, 7, 4, isWhiteTurn);
-        tileBoard[7][5] = new Bishop(oppositeColor, 7, 5, isWhiteTurn);
-        tileBoard[7][6] = new Knight(oppositeColor, 7, 6, isWhiteTurn);
-        tileBoard[7][7] = new Rook(oppositeColor, 7, 7, isWhiteTurn);
+        tileBoard[7][0] = new Rook(oppositeColor, 7, 0, white_pieces_at_bottom);
+        tileBoard[7][1] = new Knight(oppositeColor, 7, 1, white_pieces_at_bottom);
+        tileBoard[7][2] = new Bishop(oppositeColor, 7, 2, white_pieces_at_bottom);
+        tileBoard[7][3] = new Queen(oppositeColor, 7, 3, white_pieces_at_bottom);
+        tileBoard[7][4] = new King(oppositeColor, 7, 4, white_pieces_at_bottom);
+        tileBoard[7][5] = new Bishop(oppositeColor, 7, 5, white_pieces_at_bottom);
+        tileBoard[7][6] = new Knight(oppositeColor, 7, 6, white_pieces_at_bottom);
+        tileBoard[7][7] = new Rook(oppositeColor, 7, 7, white_pieces_at_bottom);
         for (int j = 0; j < 8; j++) {
-            tileBoard[6][j] = new Pawn(oppositeColor, 6, j, isWhiteTurn);
+            tileBoard[6][j] = new Pawn(oppositeColor, 6, j, white_pieces_at_bottom);
         }
 
     }
