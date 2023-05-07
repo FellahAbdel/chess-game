f1d42a719aed12765a6001afdc86c1fd7b3a6fd1
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-05-07 14:58:28 +0200

    resetHasJustMoveDouble() and setHasJustMoveDouble()

--- src/com/diaby/model/ChessBoard.java
+++ src/com/diaby/model/ChessBoard.java
@@ -13,41 +13,42 @@ public class ChessBoard {
 
     public void initialize(boolean isWhiteTurn) {
         Color color = isWhiteTurn ? Color.BLACK : Color.WHITE;
-        Color oppositeColor = isWhiteTurn ? Color.WHITE : Color.BLACK ;
+        Color oppositeColor = isWhiteTurn ? Color.WHITE : Color.BLACK;
 
         // Initialiser les pièces blanches
         tileBoard[0][0] = new Rook(color, 0, 0, isWhiteTurn);
-        tileBoard[0][1] = new Knight(color, 0, 1,isWhiteTurn);
-        tileBoard[0][2] = new Bishop(color, 0, 2,isWhiteTurn);
-        tileBoard[0][3] = new Queen(color, 0, 3,isWhiteTurn);
-        tileBoard[0][4] = new King(color, 0, 4,isWhiteTurn);
-        tileBoard[0][5] = new Bishop(color, 0, 5,isWhiteTurn);
-        tileBoard[0][6] = new Knight(color, 0, 6,isWhiteTurn);
-        tileBoard[0][7] = new Rook(color, 0, 7,isWhiteTurn);
+        tileBoard[0][1] = new Knight(color, 0, 1, isWhiteTurn);
+        tileBoard[0][2] = new Bishop(color, 0, 2, isWhiteTurn);
+        tileBoard[0][3] = new Queen(color, 0, 3, isWhiteTurn);
+        tileBoard[0][4] = new King(color, 0, 4, isWhiteTurn);
+        tileBoard[0][5] = new Bishop(color, 0, 5, isWhiteTurn);
+        tileBoard[0][6] = new Knight(color, 0, 6, isWhiteTurn);
+        tileBoard[0][7] = new Rook(color, 0, 7, isWhiteTurn);
         for (int j = 0; j < 8; j++) {
-            tileBoard[1][j] = new Pawn(color, 1, j,isWhiteTurn);
+            tileBoard[1][j] = new Pawn(color, 1, j, isWhiteTurn);
         }
 
         // Initialiser les pièces noires
-        tileBoard[7][0] = new Rook(oppositeColor, 7, 0,isWhiteTurn);
-        tileBoard[7][1] = new Knight(oppositeColor, 7, 1,isWhiteTurn);
-        tileBoard[7][2] = new Bishop(oppositeColor, 7, 2,isWhiteTurn);
-        tileBoard[7][3] = new Queen(oppositeColor, 7, 3,isWhiteTurn);
-        tileBoard[7][4] = new King(oppositeColor, 7, 4,isWhiteTurn);
-        tileBoard[7][5] = new Bishop(oppositeColor, 7, 5,isWhiteTurn);
-        tileBoard[7][6] = new Knight(oppositeColor, 7, 6,isWhiteTurn);
-        tileBoard[7][7] = new Rook(oppositeColor, 7, 7,isWhiteTurn);
+        tileBoard[7][0] = new Rook(oppositeColor, 7, 0, isWhiteTurn);
+        tileBoard[7][1] = new Knight(oppositeColor, 7, 1, isWhiteTurn);
+        tileBoard[7][2] = new Bishop(oppositeColor, 7, 2, isWhiteTurn);
+        tileBoard[7][3] = new Queen(oppositeColor, 7, 3, isWhiteTurn);
+        tileBoard[7][4] = new King(oppositeColor, 7, 4, isWhiteTurn);
+        tileBoard[7][5] = new Bishop(oppositeColor, 7, 5, isWhiteTurn);
+        tileBoard[7][6] = new Knight(oppositeColor, 7, 6, isWhiteTurn);
+        tileBoard[7][7] = new Rook(oppositeColor, 7, 7, isWhiteTurn);
         for (int j = 0; j < 8; j++) {
             tileBoard[6][j] = new Pawn(oppositeColor, 6, j, isWhiteTurn);
         }
 
     }
+
     public static ChessPiece[][] copyBoard(ChessPiece[][] board) {
         ChessPiece[][] copy = new ChessPiece[8][8];
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 ChessPiece piece = board[row][col];
-                if (piece != null ) {
+                if (piece != null) {
                     ChessPiece newPiece;
                     if (piece instanceof Pawn) {
                         newPiece = new Pawn(piece);
@@ -74,10 +75,11 @@ public class ChessBoard {
     }
 
     public void resetHighlight() {
-        for(int i = 0; i< 8; i++)
-            for(int j = 0; j< 8; j++)
-                highLightCase[i][j]=false;
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++)
+                highLightCase[i][j] = false;
     }
+
     public ChessPiece getPieceAt(int row, int col) {
         return tileBoard[row][col];
     }
@@ -91,7 +93,7 @@ public class ChessBoard {
         if (pieceToRemove == null) {
             throw new IllegalArgumentException("No piece at " + row + " " + col);
         }
-        tileBoard[row][col] = null ;
+        tileBoard[row][col] = null;
     }
 
     public boolean isOccupied(int row, int col) {
@@ -111,14 +113,13 @@ public class ChessBoard {
         return piecesList;
     }
 
-    public King getKing(boolean isWhiteKing)
-    {
+    public King getKing(boolean isWhiteKing) {
         King king = null;
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
                 ChessPiece piece = tileBoard[row][col];
                 if (piece instanceof King && piece.isWhite() == isWhiteKing) {
-                    king = (King) getPieceAt(row,col);
+                    king = (King) getPieceAt(row, col);
                 }
             }
         }
@@ -126,11 +127,21 @@ public class ChessBoard {
         return king;
     }
 
+    public void resetBooleanPawn(boolean isWhitePiece) {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                ChessPiece piece = tileBoard[row][col];
+                if (piece instanceof Pawn && piece.isWhite() == isWhitePiece) {
+                    ((Pawn) piece).setHasJustMoveDouble(false);
+                }
+            }
+        }
+    }
+
     public void move(int startRow, int startCol, int endRow, int endCol) {
         ChessPiece startPiece = getPieceAt(startRow, startCol);
         ChessPiece endPiece = getPieceAt(endRow, endCol);
-        if(endPiece != null && (endPiece.getColor() != startPiece.getColor()))
-        {
+        if (endPiece != null && (endPiece.getColor() != startPiece.getColor())) {
             endPiece.setCaptured();
             removePieceAt(endRow, endCol);
         }
@@ -153,17 +164,15 @@ public class ChessBoard {
         }
 
         // capture en passant
-        if(!startPiece.getWhiteTurn())
-        {
-            if ( startPiece instanceof Pawn && endCol != startCol && endPiece == null) {
+        if (!startPiece.getWhiteTurn()) {
+            if (startPiece instanceof Pawn && endCol != startCol && endPiece == null) {
                 int capturedPieceRow = startPiece.isWhite() ? endRow - 1 : endRow + 1;
                 ChessPiece capturedPiece = getPieceAt(capturedPieceRow, endCol);
                 capturedPiece.setCaptured();
                 removePieceAt(capturedPieceRow, endCol);
             }
-        }
-        else{
-            if ( startPiece instanceof Pawn && endCol != startCol && endPiece == null) {
+        } else {
+            if (startPiece instanceof Pawn && endCol != startCol && endPiece == null) {
                 int capturedPieceRow = !startPiece.isWhite() ? endRow - 1 : endRow + 1;
                 ChessPiece capturedPiece = getPieceAt(capturedPieceRow, endCol);
                 capturedPiece.setCaptured();
@@ -173,10 +182,15 @@ public class ChessBoard {
 
 
         move(startRow, startCol, endRow, endCol);
+
+        if (startPiece instanceof Pawn && Math.abs(startRow - endRow) == 2) {
+            ((Pawn) startPiece).setHasJustMoveDouble(true);
+        }
+
         // Mise à hasMoved = true, une fois le déplacement effectué pour le roi et la tour.
-        if(startPiece instanceof King){
+        if (startPiece instanceof King) {
             ((King) startPiece).setHasMoved(true);
-        }else if (startPiece instanceof Rook){
+        } else if (startPiece instanceof Rook) {
             ((Rook) startPiece).setHasMoved(true);
         }
         return true;
