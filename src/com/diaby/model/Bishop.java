48cbc6ea59f37346f923a342072b86108e6a31cf
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2023-05-07 15:24:39 +0200

    éffacement des break dans les recherches des positions possibles

--- src/com/diaby/model/Bishop.java
+++ src/com/diaby/model/Bishop.java
@@ -16,52 +16,55 @@ public class Bishop extends ChessPiece {
     //    @Override
     public ArrayList<int[]> possiblesMoves(int startYRow, int startXCol, ChessPiece[][] board) {
         ArrayList<int[]> moves = new ArrayList<>();
-
+        boolean canMove = true;
         // Upper Left Diagonal
-        for (int i = startYRow - 1, j = startXCol - 1; i >= 0 && j >= 0; i--, j--) {
+        for (int i = startYRow - 1, j = startXCol - 1; i >= 0 && j >= 0 && canMove; i--, j--) {
             if (board[i][j] == null) {
                 moves.add(new int[]{i, j});
             } else if (board[i][j].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, j});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
+        canMove = true;
         // Upper Right Diagonal
-        for (int i = startYRow - 1, j = startXCol + 1; i >= 0 && j < 8; i--, j++) {
+        for (int i = startYRow - 1, j = startXCol + 1; i >= 0 && j < 8 && canMove; i--, j++) {
             if (board[i][j] == null) {
                 moves.add(new int[]{i, j});
             } else if (board[i][j].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, j});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
+        canMove = true;
         // Lower Left Diagonal
-        for (int i = startYRow + 1, j = startXCol - 1; i < 8 && j >= 0; i++, j--) {
+        for (int i = startYRow + 1, j = startXCol - 1; i < 8 && j >= 0 && canMove; i++, j--) {
             if (board[i][j] == null) {
                 moves.add(new int[]{i, j});
             } else if (board[i][j].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, j});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
+        canMove = true;
         // Lower Right Diagonal
-        for (int i = startYRow + 1, j = startXCol + 1; i < 8 && j < 8; i++, j++) {
+        for (int i = startYRow + 1, j = startXCol + 1; i < 8 && j < 8 && canMove; i++, j++) {
             if (board[i][j] == null) {
                 moves.add(new int[]{i, j});
             } else if (board[i][j].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, j});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
--- src/com/diaby/model/Queen.java
+++ src/com/diaby/model/Queen.java
@@ -16,103 +16,109 @@ public class Queen extends ChessPiece {
 
     public ArrayList<int[]> possiblesMoves(int startYRow, int startXCol, ChessPiece[][] board) {
         ArrayList<int[]> moves = new ArrayList<>();
-
+        boolean canMove = true;
         // generate moves like bishop
         // Upper Left Diagonal
-        for (int i = startYRow - 1, j = startXCol - 1; i >= 0 && j >= 0; i--, j--) {
+        for (int i = startYRow - 1, j = startXCol - 1; i >= 0 && j >= 0 && canMove; i--, j--) {
             if (board[i][j] == null) {
                 moves.add(new int[]{i, j});
             } else if (board[i][j].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, j});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
+        canMove = true;
         // Upper Right Diagonal
-        for (int i = startYRow - 1, j = startXCol + 1; i >= 0 && j < 8; i--, j++) {
+        for (int i = startYRow - 1, j = startXCol + 1; i >= 0 && j < 8 && canMove; i--, j++) {
             if (board[i][j] == null) {
                 moves.add(new int[]{i, j});
             } else if (board[i][j].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, j});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
+        canMove = true;
         // Lower Left Diagonal
-        for (int i = startYRow + 1, j = startXCol - 1; i < 8 && j >= 0; i++, j--) {
+        for (int i = startYRow + 1, j = startXCol - 1; i < 8 && j >= 0 && canMove; i++, j--) {
             if (board[i][j] == null) {
                 moves.add(new int[]{i, j});
             } else if (board[i][j].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, j});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
+        canMove = true;
         // Lower Right Diagonal
-        for (int i = startYRow + 1, j = startXCol + 1; i < 8 && j < 8; i++, j++) {
+        for (int i = startYRow + 1, j = startXCol + 1; i < 8 && j < 8 && canMove; i++, j++) {
             if (board[i][j] == null) {
                 moves.add(new int[]{i, j});
             } else if (board[i][j].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, j});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
         // generate moves like rook
-
+        canMove = true;
         // Check moves to the right
-        for (int i = startXCol + 1; i < 8; i++) {
+        for (int i = startXCol + 1; i < 8 && canMove; i++) {
             if (board[startYRow][i] == null) {
                 moves.add(new int[]{startYRow, i});
             } else if (board[startYRow][i].isWhite() != this.isWhite()) {
                 moves.add(new int[]{startYRow, i});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
+        canMove = true;
         // Check moves to the left
-        for (int i = startXCol - 1; i >= 0; i--) {
+        for (int i = startXCol - 1; i >= 0 && canMove; i--) {
             if (board[startYRow][i] == null) {
                 moves.add(new int[]{startYRow, i});
             } else if (board[startYRow][i].isWhite() != this.isWhite()) {
                 moves.add(new int[]{startYRow, i});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
+        canMove = true;
         // Check moves to the bottom
-        for (int i = startYRow + 1; i < 8; i++) {
+        for (int i = startYRow + 1; i < 8 && canMove; i++) {
             if (board[i][startXCol] == null) {
                 moves.add(new int[]{i, startXCol});
             } else if (board[i][startXCol].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, startXCol});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
+        canMove = true;
         // Check moves to the top
-        for (int i = startYRow - 1; i >= 0; i--) {
+        for (int i = startYRow - 1; i >= 0 && canMove; i--) {
             if (board[i][startXCol] == null) {
                 moves.add(new int[]{i, startXCol});
             } else if (board[i][startXCol].isWhite() != this.isWhite()) {
                 moves.add(new int[]{i, startXCol});
-                break;
+                canMove = false;
             } else {
-                break;
+                canMove = false;
             }
         }
 
